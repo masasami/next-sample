17ab580a89e311ac13601a136a29dab17a88164c
masasami <masangular0518@gmail.com>
2021-09-28 00:56:56 +0900

    オートコレクトリスト

---
+++ pages/component0024.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+export default function Component() {
+  const flavors = ['チョコレート', 'イチゴ', 'バナナ', 'バニラ']
+  const [selectedFlavor, setSelectedFlavor] = useState('')
+
+  return (
+    <div>
+      <h2>アイスの味：{selectedFlavor}</h2>
+      <input
+        type="text"
+        list="flavors"
+        value={selectedFlavor}
+        onChange={(e) => setSelectedFlavor(e.target.value)}
+      />
+      <datalist id="flavors">
+        {flavors.map((flavor, i) => (
+          <option key={i} value={flavor} />
+        ))}
+      </datalist>
+    </div>
+  )
+}
