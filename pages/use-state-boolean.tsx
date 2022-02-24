765152142672c77cc5991d640e7d2cd72d8ff483
masasami <masangular0518@gmail.com>
2022-02-25 03:08:02 +0900

    スタイルのファイル変更

--- pages/use-state-boolean.tsx
+++ pages/use-state-boolean.tsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import styles from './component.module.scss'
+import styles from '../styles/use-state-boolean.module.scss'
 
 export default function Component() {
   const [flag, setFlag] = useState(false)
 
   return (
     <div>
-      <h1 className={flag ? styles.active : ''}>
-        フラグに応じて色が変わります
-      </h1>
+      <h1 className={flag ? styles.active : ''}>フラグに応じて色が変わります</h1>
       <button onClick={() => setFlag(!flag)}>フラグ切り替え</button>
     </div>
   )
