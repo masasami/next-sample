f881dbde75b410bd1eeb3bf68d4b369c02d68834
masasami <masavel0518@gmail.com>
2022-07-20 02:09:09 +0900

    デバイスのカメラで撮影

---
+++ pages/device-camera.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useRef } from 'react'
+
+const constraints: MediaStreamConstraints = {
+  audio: false,
+  video: {
+    width: 300,
+    height: 200,
+    facingMode: 'user',
+    // facingMode: { exact: 'environment' },
+  },
+}
+
+export default function Component() {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  const handleClickShutter = () => {
+    const ctx = canvasRef.current.getContext('2d')
+
+    videoRef.current.pause()
+    setTimeout(() => {
+      videoRef.current.play()
+    }, 500)
+
+    ctx.drawImage(videoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height)
+  }
+
+  useEffect(() => {
+    ;(async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints)
+        videoRef.current.srcObject = stream
+        videoRef.current.onloadedmetadata = () => {
+          videoRef.current.play()
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    })()
+  }, [])
+
+  return (
+    <div>
+      <video ref={videoRef} width={300} height={200}></video>
+      <canvas ref={canvasRef} width={300} height={200}></canvas>
+      <button onClick={handleClickShutter}>撮影する</button>
+    </div>
+  )
+}
